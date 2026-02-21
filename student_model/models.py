0325081d912685a788bf511adeb2117174ae43a7
rubaiiyat <abirr1516@gmail.com>
2026-02-21 17:55:03 +0600

    first commit

---
+++ student_model/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=50)
+    roll=models.IntegerField(unique=True)
+    address=models.TextField()
+    fathers_name=models.CharField(default="Unknown",max_length=50)
+    mothers_name=models.CharField(default="Unknown",max_length=50)
+
+    def __str__(self):
+        return f'Roll: {self.roll} | and  Name: { self.name}'
